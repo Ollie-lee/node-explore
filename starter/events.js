1cd5fad8e2463c8bc8c66b321ac0376960479b17
Ollie <myself.ollie.lee@gmail.com>
2020-10-28 13:08:13 +1100

    event module

--- starter/events.js
+++ starter/events.js
@@ -1,6 +1,11 @@
 const EventEmitter = require("events");
-
-const myEmitter = new EventEmitter();
+const http = require("http");
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
   //the callback function
@@ -8,10 +13,32 @@ myEmitter.on("newSale", () => {
   console.log("There is a new sale");
 });
 
-myEmitter.on("newSale", () => {
-  console.log("Customer name: ollie");
+myEmitter.on("newSale", (stock) => {
+  console.log("Customer name: ollie", stock);
 });
 
 // emit an event called newSale
 //this emitting here is as if we were clicking on the button,
-myEmitter.emit("newSale");
+// myEmitter.emit("newSale", 10);
+
+////////////////////////////////////////////////////////////////////
+const server = http.createServer();
+//listen to different events that server emits
+server.on("request", (request, res) => {
+  console.log("request received");
+  console.log(1, request.url);
+  res.end("Request received");
+});
+
+server.on("request", (request, res) => {
+  // console.log(2, request.url);
+  console.log("another request received");
+});
+
+server.on("close", () => {
+  console.log("server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for request...");
+});
